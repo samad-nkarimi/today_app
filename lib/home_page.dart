d0e7adb93dcf5abb372d8c9db40d535d000c6a4d
samad karimi <samad.nkarimi@gmail.com>
2021-11-30 12:31:38 +0700

    some change in notes shownig

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,11 @@
 import 'dart:ui';
 
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:today_app/Form_widget.dart';
+import 'package:today_app/blocs/note/note.dart';
 import 'package:today_app/calender_page.dart';
 import 'package:today_app/draw_arc.dart';
 import 'package:today_app/drawer_widget.dart';
@@ -17,89 +20,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int noteCount = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       endDrawer: const DrawerWidget(),
-      body: Builder(
-        builder: (BuildContext context) {
-          return SingleChildScrollView(
-            child: Stack(children: [
-              Container(
-                color: Colors.white,
-                padding: const EdgeInsets.only(bottom: 50),
-                child: Column(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(bottom: 100 / 2 + 20),
-                      // color: Colors.blue,
-                      child: ShaderMask(
-                        shaderCallback: (rect) {
-                          return const LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            stops: [
-                              0.6,
-                              1.3,
-                            ],
-                            colors: [Colors.black, Colors.transparent],
-                          ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
-                        },
-                        blendMode: BlendMode.dstIn,
-                        child: Image.asset(
-                          "assets/images/fall.gif",
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                      height: 300,
-                      width: double.infinity,
+      body: Builder(builder: (BuildContext context) {
+        return SingleChildScrollView(
+          child: Stack(
+            children: [
+              BlocBuilder<NoteBloc, NoteState>(
+                builder: (context, state) {
+                  return Container(
+                    color: Colors.white,
+                    padding: const EdgeInsets.only(bottom: 50),
+                    child: Column(
+                      children: [
+                        topPicture(),
+                        if (noteCount == 0) noNoteWidget(),
+                        for (int i = 0; i < noteCount; i++) const NoteItem(labelColor: Colors.orange),
+                        addNoteWidgetButton(),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                          height: 150,
+                          width: double.infinity,
+                          child: const Text(""),
+                          color: Colors.grey,
+                        )
+                      ],
                     ),
-                    const NoteItem(labelColor: Colors.orange),
-                    const NoteItem(labelColor: Colors.green),
-                    const NoteItem(labelColor: Colors.red),
-                    // NoteItem(),
-                    ElevatedButton(
-                      onPressed: () {
-                        showModalBottomSheet(
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(30.0),
-                              topRight: Radius.circular(30.0),
-                            ),
-                          ),
-                          context: context,
-                          builder: (context) {
-                            return Container(
-                              // height: 300,
-                              color: Colors.transparent,
-                              child: const FormWidget(),
-                            );
-                          },
-                        );
-                      },
-                      child: const Text("add note"),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-                      height: 150,
-                      width: double.infinity,
-                      child: const Text(""),
-                      color: Colors.grey,
-                    )
-                  ],
-                ),
+                  );
+                },
               ),
-              Positioned(
-                right: 20,
-                top: 20,
-                child: IconButton(
-                  onPressed: () {
-                    print("pressed");
-                    Scaffold.of(context).openEndDrawer();
-                  },
-                  icon: SvgPicture.asset("assets/images/menu_icon.svg"),
-                ),
-              ),
+              menuButton(),
               Padding(
                 padding: const EdgeInsets.only(top: 300 - 100 / 2),
                 child: Row(
@@ -161,10 +115,10 @@ class _HomePageState extends State<HomePage> {
                   ],
                 ),
               ),
-            ]),
-          );
-        }
-      ),
+            ],
+          ),
+        );
+      }),
       floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
       floatingActionButton: InkWell(
         onTap: () => Navigator.pushNamed(context, CalendarPage.routeName),
@@ -186,5 +140,96 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-}
 
+  Widget noNoteWidget() {
+    return Container(
+      height: 50,
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5.0),
+        color: Colors.grey.withOpacity(0.2),
+      ),
+      child: DottedBorder(
+        borderType: BorderType.RRect,
+        radius: const Radius.circular(5),
+        dashPattern: const [10, 5],
+        color: Colors.grey,
+        strokeWidth: 1.5,
+        child: Container(
+          alignment: Alignment.center,
+          child: const Text("no note"),
+        ),
+      ),
+    );
+  }
+
+  Widget topPicture() {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 100 / 2 + 20),
+      // color: Colors.blue,
+      child: ShaderMask(
+        shaderCallback: (rect) {
+          return const LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [
+              0.9,
+              1.1,
+            ],
+            colors: [Colors.black, Colors.transparent],
+          ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+        },
+        blendMode: BlendMode.dstIn,
+        child: Image.asset(
+          "assets/images/fall.gif",
+          fit: BoxFit.fill,
+        ),
+      ),
+      height: 300,
+      width: double.infinity,
+    );
+  }
+
+  Widget menuButton() {
+    return Positioned(
+      right: 0,
+      top: 20,
+      child: Container(
+        height: 70,
+        width: 70,
+        child: IconButton(
+          onPressed: () {
+            print("pressed");
+            Scaffold.of(context).openEndDrawer();
+          },
+          icon: SvgPicture.asset("assets/images/menu_icon.svg"),
+        ),
+      ),
+    );
+  }
+
+  Widget addNoteWidgetButton() {
+    return ElevatedButton(
+      onPressed: () {
+        showModalBottomSheet(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              topRight: Radius.circular(30.0),
+            ),
+          ),
+          context: context,
+          builder: (context) {
+            return Container(
+              // height: 300,
+              color: Colors.transparent,
+              child: const FormWidget(),
+            );
+          },
+        );
+      },
+      child: const Text("add note"),
+    );
+  }
+}
